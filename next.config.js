8a97766301d5e99defaf746cf34bbeb0ef089b96
Luca Milanese <luca.milanese90@gmail.com>
2023-11-17 11:34:36 +0100

    feat: update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.(tsx|ts)$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    })
+
     config.resolve.fallback = { fs: false, net: false, tls: false }
+
     return config
   },
 }
